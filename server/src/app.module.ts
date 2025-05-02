4da1a4859e680d9dd58be4ad3996394a1a2935a2
zlatonn <tonkar2541@hotmail.com>
2025-05-02 18:31:21 +0700

    feat: update app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common"
 import { OfficesModule } from "./offices/offices.module"
+import { ServicesModule } from "./services/services.module"
 
 @Module({
-	imports: [OfficesModule],
+	imports: [OfficesModule, ServicesModule],
 	controllers: [],
 	providers: [],
 })
