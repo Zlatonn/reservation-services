cac88b91377dc4938d3491f7449b8d31dc9a49c3
zlatonn <tonkar2541@hotmail.com>
2025-05-03 16:18:58 +0700

    feat: implement sidebar component
    - create side bar content
    - create app-sidebar
    - edit sidebar config

---
+++ client/src/components/app-sidebar.tsx
@@ -0,0 +1,58 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import { menuItems } from "@/contents/sidebar-content";
+import { LogOut } from "lucide-react";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader
+        style={{
+          backgroundImage: `linear-gradient(120deg, 
+      var(--sidebar-primary) 0%, 
+      var(--sidebar-primary) 25%,  
+      var(--sidebar-primary-shade) 55%,
+      var(--sidebar-primary-shade) 60%, 
+      var(--sidebar-primary) 100%)`,
+        }}
+      >
+        <div className="flex justify-center items-center px-5 gap-3">
+          <img src="/public/images/logo.svg" alt="Logo" className="w-12 h-12" />
+          <p className="text-lg font-medium text-accent">ระบบจัดการจอง</p>
+        </div>
+      </SidebarHeader>
+
+      <SidebarContent className="px-3 py-5 text-muted-foreground">
+        <SidebarMenu className="gap-3">
+          {menuItems.map((item, i) => (
+            <SidebarMenuItem key={`item-${i}`}>
+              <SidebarMenuButton asChild className="py-5 shadow-xs">
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenuButton asChild className="flex justify-center items-center bg-primary text-accent hover:bg-primary cursor-pointer">
+          <a href="#">
+            <LogOut />
+            <span>ออกจากระบบ</span>
+          </a>
+        </SidebarMenuButton>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ client/src/contents/sidebar-content.tsx
@@ -0,0 +1,50 @@
+import { LayoutDashboard, UserPlus, Users, FileCog, CalendarCog, FileChartLine, PanelsTopLeft, FileImage } from "lucide-react";
+
+// Menu items.
+export const menuItems = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "ขอสิทธิ์ใช้งาน",
+    url: "#",
+    icon: UserPlus,
+  },
+  {
+    title: "ผู้ใช้งานทั้งหมด",
+    url: "#",
+    icon: Users,
+  },
+  {
+    title: "กำหนดประเภทงานของระบบ",
+    url: "#",
+    icon: FileCog,
+  },
+  {
+    title: "กำหนดกลุ่มงานของระบบ",
+    url: "#",
+    icon: FileCog,
+  },
+  {
+    title: "ปิดวันหยุดราชการ",
+    url: "#",
+    icon: CalendarCog,
+  },
+  {
+    title: "รายงานสถิติ",
+    url: "#",
+    icon: FileChartLine,
+  },
+  {
+    title: "อัพโหลดประชาสัมพันธ์",
+    url: "#",
+    icon: FileImage,
+  },
+  {
+    title: "จัดการสำนักงาน",
+    url: "#",
+    icon: PanelsTopLeft,
+  },
+];
