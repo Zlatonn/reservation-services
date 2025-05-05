836024a3773f66de9024d138b3a14d311ba216da
zlatonn <tonkar2541@hotmail.com>
2025-05-05 18:28:55 +0700

    feat: implement use-dialog

---
+++ client/src/hooks/use-form-dialog.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+interface FormDialog {
+  isOpen: boolean;
+  id: string;
+  openDialog: (id?: string) => void;
+  closeDialog: () => void;
+}
+
+export const useFormDialog = create<FormDialog>((set) => ({
+  isOpen: false,
+  id: "",
+  openDialog: (id) => set({ isOpen: true, id }),
+  closeDialog: () => set({ isOpen: false, id: "" }),
+}));
