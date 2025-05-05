4cf4d3580881420811169e5c759cee89826986c6
zlatonn <tonkar2541@hotmail.com>
2025-05-06 02:49:15 +0700

    chore: enableCors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,9 @@ import { GlobalExceptionFilter } from "./common/filters/exception.filter"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.enableCors({
+		origin: "http://localhost:5173",
+	})
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
