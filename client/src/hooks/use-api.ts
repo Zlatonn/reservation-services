b54f76ddd5e7970a861ce3552ab8951fdc60a2a3
zlatonn <tonkar2541@hotmail.com>
2025-05-09 04:48:10 +0700

    feat: implement service category & service name api

--- client/src/hooks/use-api.ts
+++ client/src/hooks/use-api.ts
@@ -128,6 +128,7 @@ interface Service {
   openingDays: OpenningDay[];
   description?: string;
 }
+
 // Service api
 export const useCreateService = () => {
   const queryClient = useQueryClient();
@@ -194,6 +195,167 @@ export const useDeleteService = (id: string) => {
     },
     onError: (error) => {
       console.log("Error delete service: ", error);
+    },
+  });
+};
+
+interface ServiceCateogry {
+  name: string;
+}
+
+// Category api
+export const useCreateServiceCategory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["createServiceCategory"],
+    mutationFn: async (newServiceCategory: ServiceCateogry) => {
+      const response = await axiosClient.post(`/service-categories`, newServiceCategory);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      // Invalidate and force refetch offices query
+      queryClient.invalidateQueries({ queryKey: ["serviceCategories"] });
+    },
+    onError: (error) => {
+      console.log("Error create service category: ", error);
+    },
+  });
+};
+
+export const useGetServiceCategories = () => {
+  return useQuery({
+    queryKey: ["serviceCategories"],
+    queryFn: async () => {
+      try {
+        const response = await axiosClient.get(`/service-categories`);
+        return response.data.data;
+      } catch (error) {
+        console.error("Error fetching service categories: ", error);
+      }
+    },
+  });
+};
+
+export const useUpdateServiceCategory = (id: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["updateServiceCategory", id],
+    mutationFn: async (updateServiceCategory: ServiceCateogry) => {
+      const response = await axiosClient.patch(`/service-categories/${id}`, updateServiceCategory);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      // Invalidate and force refetch offices query
+      queryClient.invalidateQueries({ queryKey: ["serviceCategories"] });
+    },
+    onError: (error) => {
+      console.log("Error update service category: ", error);
+    },
+  });
+};
+
+export const useDeleteServiceCategory = (id: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["deleteServiceCategory", id],
+    mutationFn: async () => {
+      const response = await axiosClient.delete(`/service-categories/${id}`);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      // Invalidate and force refetch offices query
+      queryClient.invalidateQueries({ queryKey: ["serviceCategories"] });
+    },
+    onError: (error) => {
+      console.log("Error delete service category: ", error);
+    },
+  });
+};
+
+export const useGetAllServiceNamesByServiceCategoryId = (serviceCategoryId: string) => {
+  return useQuery({
+    queryKey: ["serviceNameByServiceCategoryId", serviceCategoryId],
+    queryFn: async () => {
+      try {
+        const response = await axiosClient.get(`/service-categories/${serviceCategoryId}/service-names`);
+        return response.data.data;
+      } catch (error) {
+        console.error("Error fetching service by office ID: ", error);
+      }
+    },
+    enabled: !!serviceCategoryId,
+  });
+};
+
+interface ServiceName {
+  name: string;
+}
+
+// Name api
+export const useCreateServiceName = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["createServiceName"],
+    mutationFn: async (newServiceName: ServiceName) => {
+      const response = await axiosClient.post(`/service-names`, newServiceName);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      // Invalidate and force refetch offices query
+      queryClient.invalidateQueries({ queryKey: ["serviceNames"] });
+    },
+    onError: (error) => {
+      console.log("Error create service name: ", error);
+    },
+  });
+};
+
+export const useGetServiceNames = () => {
+  return useQuery({
+    queryKey: ["serviceNames"],
+    queryFn: async () => {
+      try {
+        const response = await axiosClient.get(`/service-names`);
+        return response.data.data;
+      } catch (error) {
+        console.error("Error fetching service names: ", error);
+      }
+    },
+  });
+};
+
+export const useUpdateServiceName = (id: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["updateServiceName", id],
+    mutationFn: async (updateServiceName: ServiceName) => {
+      const response = await axiosClient.patch(`/service-names/${id}`, updateServiceName);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      // Invalidate and force refetch offices query
+      queryClient.invalidateQueries({ queryKey: ["serviceNames"] });
+    },
+    onError: (error) => {
+      console.log("Error update service name: ", error);
+    },
+  });
+};
+
+export const useDeleteServiceName = (id: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["deleteServiceName", id],
+    mutationFn: async () => {
+      const response = await axiosClient.delete(`/service-names/${id}`);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      // Invalidate and force refetch offices query
+      queryClient.invalidateQueries({ queryKey: ["serviceNames"] });
+    },
+    onError: (error) => {
+      console.log("Error delete service name: ", error);
     },
   });
 };
